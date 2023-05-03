82733ce20cd42ae63338a5b2695a63d23023d1d7
KevinGuedes <kevinguedes@edu.unifor.br>
2023-05-03 12:52:24 -0300

    feat: main components base structure created

--- src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <main>
-    <p>sdads</p>
-    <h1>asdsald</h1>
-  </main>
-</template>
---
+++ src/views/PostDetailsView/PostDetailsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Post Details</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
